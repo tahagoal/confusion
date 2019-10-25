212b427f0d58c796d2433b3eb8cdcd38632a09ac
tahagoal <tahagoal@gmail.com>
2019-10-25 20:48:42 +0200

    functional

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -21,14 +21,16 @@ class DishDetail extends Component {
         if (comments.length > 0) {
             return comments.map((comment) => {
                 if (comment != null) {
-                    return (<div>
-                        <p>
-                            {comment.comment}
-                        </p>
-                        <p>
-                            -- {comment.author}, {comment.date}
-                        </p>
-                    </div>
+                    return (
+
+                        <div>
+                            <p>
+                                {comment.comment}
+                            </p>
+                            <p>
+                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>
                     )
                 }
                 else {
@@ -37,49 +39,31 @@ class DishDetail extends Component {
                     )
                 }
             })
-            // comments.forEach(element => {
-            //     if (element != null) {
-            //         console.log(element);
-            //         array.push(
-            //             <div>
-            //                 <p>
-            //                     {element.comment}
-            //                 </p>
-            //                 <p>
-            //                     -- {element.author}, {element.date}
-            //                 </p>
-            //             </div>
-            //         )
-
-            //     }
-            //     else {
-            //         array.push(
-            //             <div></div>
-            //         );
-            //     }
-            // });
-            // return array;
         }
     }
 
     renderDish(dish) {
         if (dish != null) {
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        {this.renderComments(dish.comments)}
+                <div className="container">
+
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            {this.renderComments(dish.comments)}
+                        </div>
                     </div>
                 </div>
+
             );
         }
         else {
